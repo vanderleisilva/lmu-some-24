d6b59249f6f3a9b249f71360cec5889c8a39ddea
Vanderlei Alves da Silva <vanderlei.alves-da-silva@regiondo.com>
2024-06-24 14:20:09 +0200

    Update lines exercise

--- web/canvas/lines/lines.js
+++ web/canvas/lines/lines.js
@@ -7,28 +7,20 @@ const interval = 10;
 const numberOfLines = Math.floor(canvas.height / interval);
 
 // Draw lines
-context.strokeStyle = "red";
-
-// context.beginPath();
-// // x, y
-// context.moveTo(10, 20);
-// // width, height
-// context.lineTo(100, 200);
-// context.stroke();
-
-
-// context.beginPath();
-// // x, y
-// context.moveTo(200, 20);
-// // width, height
-// context.lineTo(100, 200);
-// context.stroke();
 
 for (let i = 0; i < numberOfLines; i++) {
   context.beginPath();
-  // x, y
+
+  // left lines x, y
+  context.strokeStyle = "red";
   context.moveTo(0, interval * i);
-  // width, height
   context.lineTo(interval * i, canvas.height);
+  context.stroke();
+
+  // right lines x, y
+  context.beginPath();
+  context.strokeStyle = "blue";
+  context.moveTo(canvas.width, interval * i);
+  context.lineTo(canvas.width - interval * i, canvas.height);
   context.stroke();
 }
